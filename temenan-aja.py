5b9954e14c933049819d31b896ca1f55a8bf4871
Rennn <145334508+RenAllagan@users.noreply.github.com>
2023-09-21 17:32:16 +0700

    Add files via upload

---
+++ temenan-aja.py
@@ -0,0 +1,10 @@
+aku_badut = False
+dia_spesial = True
+oranglain_badut = True
+
+if (aku_badut and dia_spesial):
+    print("awokawok, kamu lucu ya")
+elif (oranglain_badut and dia_spesial):
+    print("kamu terlalu serius")
+else:
+    print("dhlh, temenan aja deh")
